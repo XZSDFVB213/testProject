b0e372f66ef3d35310a1162e566a4646a4d4a5cd
XZSDFVB213 <abakarmadatov2@gmail.com>
2024-11-11 10:19:16 +0300

    первый коммит, нужно пофиксить мобильую шапку

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  mobileValue:boolean = false
+  mobileArrow:boolean = false
+  openMenu(){
+    this.mobileValue = !this.mobileValue
+  }
+  openArrow(){
+    this.mobileArrow = !this.mobileArrow
+  }
+}
